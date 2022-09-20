79ae4a1bb51d4029c2111f7d8985fefd3a34628b
renthus <renatodasilvamaldonado@gmail.com>
2022-09-20 14:38:08 -0300

    update hashtag.py file

--- hashtag.py
+++ hashtag.py
@@ -78,3 +78,21 @@ print('#'*60)
 ##############
 #***DESAFIO***
 ##############
+
+from pathlib import Path
+import shutil
+
+estados = ['RJ','SP','MG','GO','AM']
+
+for estado in estados:
+    Path('Arquivos_Lojas/' + estado).mkdir()
+
+caminho = Path('Arquivos_Lojas/')
+arquivos = caminho.iterdir()
+for arquivo in arquivos:
+    nome_arquivo = arquivo.name
+    if nome_arquivo[-3:] == 'csv':
+        estado = nome_arquivo[-6:-4]
+        local_destino = caminho / Path(estado,nome_arquivo)
+        shutil.move(arquivo,local_destino)
+###########################################################
